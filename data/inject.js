c620fe29461fd52e00464689934c6d7724306ddd
Sean Yeh <sean.yeh@gmx.com>
2018-07-27 16:59:35 -0500

    Migrate to WebExtensions

--- data/inject.js
+++ data/inject.js
@@ -1,14 +1,23 @@
-var comic = document.getElementById("comic");
-var comicImg = document.querySelector("#comic img");
+let comic = document.getElementById("comic");
+let comicImg = document.querySelector("#comic img");
 
 if (comic){
-    var url = "http://www.explainxkcd.com/wiki/index.php" + window.location.pathname.slice(0, -1);
+    let url = "http://www.explainxkcd.com/wiki/index.php" + window.location.pathname.slice(0, -1);
 
-    comic.innerHTML += "" +
-        "<div style='font-size: 0.8em'><i>" +
-        comicImg.title +
-        "</i></div>" +
-        "<div style='font-size: 0.8em'>" +
-        "<a href=\"" + url + "\">Explain?</a>" +
-        "</div>";
+    let d = document.createElement("div");
+    d.setAttribute("style", "font-size:0.8em;");
+
+    let alt = document.createElement("div");
+    alt.setAttribute("style", "font-style:italic;");
+    alt.appendChild(document.createTextNode(comicImg.title));
+
+    let a = document.createElement("a");
+    a.setAttribute("href", url);
+    a.appendChild(document.createTextNode("Explain?"));
+
+
+    d.appendChild(alt);
+    d.appendChild(a);
+
+    comic.append(d);
 }
